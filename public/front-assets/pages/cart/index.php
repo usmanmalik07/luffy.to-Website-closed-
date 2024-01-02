33ccbd53b69301c52ee26d351a95fc632ea11615
Usman Malik <usmanmalikk2004@gmail.com>
2024-01-02 13:07:35 +0500

    first commit

---
+++ public/front-assets/pages/cart/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Shopping Cart</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+	<?php include("include/header.php"); ?>
+
+	<div class="container">
+		<div class="col-md-12">
+			<div class="row show_cart my-5">
+
+			</div>
+		</div>
+	</div>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+<script src="script.js"></script>
+
+
+
+</script>
+</body>
+</html>
