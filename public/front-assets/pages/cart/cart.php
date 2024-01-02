42ae8dd5c6105ab2e791136aa92dc20aaff4ce0b
Usman Malik <usmanmalikk2004@gmail.com>
2024-01-02 14:49:21 +0500

    2:49 pm

--- public/front-assets/pages/cart/cart.php
+++ public/front-assets/pages/cart/cart.php
@@ -13,8 +13,20 @@ session_start();
 </head>
 <body>
 
-	<?php include("include/header.php"); ?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+     	<h3><a href="../../../../front-assets/index.html" class="text-white navbar-brand">Main Page</a></h3>
+        <h3 class="text-white navbar-brand">Shopping Cart</h3>
 
+     	<div class="mr-auto"></div>
+     	<ul class="navbar-nav">
+     		<li class="nav-item text-white">
+     			<a href="index.php" class="nav-link">Item</a>
+     		</li>
+     		<li class="nav-item text-white">
+     			<a href="cart.php" class="nav-link">Cart</a>
+     		</li>
+     	</ul>
+     </nav>
 
 	<div class="container">
 		<div class="col-md-12">
